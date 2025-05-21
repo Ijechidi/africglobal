f9883eeb3d5a1f4d0fbcee122c4fcafdb7cff150
Ijechidi <chidinma.ijeom@gmail.com>
2025-05-21 21:12:03 +0000

    correct th hydration error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Navbar from '../components/Navbar';
+
 import Hero from '../components/Hero';
 import Features from '../components/Features';
 import Steps from '../components/Steps';
 import FAQ from '../components/FAQ';
 import Campaign from '../components/Campaign'
-import Footer from '../components/Footer';
+
 
 export default function Home() {
   return (
     <>
-      <Navbar />
+      
       <main className="pt-20">
         <Hero />
         <Features />
@@ -17,7 +17,7 @@ export default function Home() {
         <FAQ />
         <Campaign />
       </main>
-      <Footer />
+      
     </>
   );
 }
